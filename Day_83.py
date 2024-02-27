db12edae7ed9b83e069e6958e17fe477c88a76fb
Auspicious-EX <shubhamstudy19@gmail.com>
2024-02-27 12:34:30 +0530

    Day_83

--- Day_83.py
+++ Day_83.py
@@ -1,2 +1,15 @@
 # DAY 83 :Exercise 9 - Shoutouts to Everyone
 
+""" Write a program to pronounce list of names using win32 API. """
+
+
+import win32com.client
+
+def pronounce_names(names):
+    speaker = win32com.client.Dispatch("SAPI.SpVoice")
+    for name in names:
+        speaker.Speak("Shoutout to " + name)
+
+if __name__ == "__main__":
+    l = ["Rahul", "Nishant", "Harry"]
+    pronounce_names(l)
